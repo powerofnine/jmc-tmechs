c2bcc79d02521e6cb3150a4c2ec70a077652deb0
Arthur Uzulin <fuj1nman@hotmail.com>
2019-03-19 18:59:35 +1100

    AI Core

---
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Enemy/AI/AiManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace TMechs.Enemy.AI
+{
+    public class AiManager : MonoBehaviour
+    {
+        private AiComponent[] components;
+        
+        private void Awake()
+        {
+            components = GetComponentsInChildren<AiComponent>().OrderBy(x => x.aiIndex).ToArray();
+        }
+
+        private void Update()
+        {
+            foreach (AiComponent component in components)
+            {
+                if (component.CanExecute())
+                {
+                    component.OnAi();
+                    return;
+                }
+            }
+        }
+    }
+}
