9bb1f035048275365f1778168cdae9eb7d6a9080
Arthur Uzulin <fuj1nman@hotmail.com>
2019-03-29 13:54:24 +1100

    Update packages

--- Tyrannosaurus Mechs/Assets/Scripts/TMechs/Enemy/AI/AiManager.cs
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Enemy/AI/AiManager.cs
@@ -1,14 +1,19 @@
 ﻿using System.Linq;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace TMechs.Enemy.AI
 {
     public class AiManager : MonoBehaviour
     {
         private AiComponent[] components;
+        private PlayableGraph ai;
         
         private void Awake()
         {
+            ai = PlayableGraph.Create("Enemy AI " + name);
+            GraphVisualizerClient.Show(ai);
+            
             components = GetComponentsInChildren<AiComponent>().OrderBy(x => x.aiIndex).ToArray();
         }
 
