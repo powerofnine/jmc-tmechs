7def74f23b255d50b3eb9a2ed0963302f5e1b637
Arthur Uzulin <fuj1nman@hotmail.com>
2019-03-31 15:55:56 +1100

    Settings backend

---
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Data/Settings/CameraSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace TMechs.Data.Settings
+{
+    [SettingsProvider]
+    public class CameraSettings
+    {
+        public bool invertHorizontal;
+        public bool invertVertical;
+        public CameraMode mode;
+
+        public enum CameraMode
+        {
+            Pan,
+            Rotate
+        }
+    }
+}
