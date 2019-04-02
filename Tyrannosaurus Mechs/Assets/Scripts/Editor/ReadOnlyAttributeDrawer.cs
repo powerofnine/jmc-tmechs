6efbed677ac2c78ba9ed9347f7c7b2a82972449f
Arthur Uzulin <fuj1nman@hotmail.com>
2019-04-02 19:19:40 +1100

    Checkpoint system beginnings

---
+++ Tyrannosaurus Mechs/Assets/Scripts/Editor/ReadOnlyAttributeDrawer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMechs.InspectorAttributes;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyAttributeDrawer : DecoratorDrawer
+{
+    public override void OnGUI(Rect position)
+    {
+        GUI.enabled = false;
+    }
+
+    public override float GetHeight() => 0F;
+}
---
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Environment/Checkpoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TMechs.InspectorAttributes;
+using UnityEngine;
+
+namespace TMechs.Environment
+{
+    public class Checkpoint : MonoBehaviour
+    {
+        [Id("Unique Checkpoint ID")]
+        public string id;
+
+        private void Awake()
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                Debug.LogWarningFormat("Checkpoint {0} does not have an ID, the checkpoint will not be remembered", name);
+                id = Guid.NewGuid().ToString();
+            }
+            
+            if(CheckpointRegistry.Instance.IsRegistered(id))
+                throw new CheckpointIdNotUniqueException("Checkpoint ID of " + this + " not unique");
+                
+            CheckpointRegistry.Instance.Register(this);
+        }
+        
+        public class CheckpointIdNotUniqueException : Exception
+        {
+            public CheckpointIdNotUniqueException(string message) : base(message)
+            {
+            }
+        }
+    }
+}
