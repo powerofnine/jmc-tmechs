b911f55f650d5660ec7f0652887ea9970c69fe84
Arthur Uzulin <fuj1nman@hotmail.com>
2019-04-21 22:10:22 +1000

    Basic harrier AI and animations

---
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Enemy/AnimatorEventListener.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace TMechs.Enemy
+{
+    public class AnimatorEventListener : MonoBehaviour
+    {
+        public void OnAnimationEvent(string id)
+        {
+            foreach(IAnimatorEvent ev in GetComponentsInParent<IAnimatorEvent>())
+                ev.OnAnimationEvent(id);
+        }
+
+        public interface IAnimatorEvent
+        {
+            void OnAnimationEvent(string id);
+        }
+    }
+}
