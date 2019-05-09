a04e572cb22bb4fd8d6d3c346f07195efa1f0530
Arthur Uzulin <fuj1nman@hotmail.com>
2019-05-10 07:58:50 +1000

    Camera shake
    Closes #5

---
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Player/CameraShake.cs
@@ -0,0 +1,37 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace TMechs.Player
+{
+    public class CameraShake : MonoBehaviour
+    {
+        public Transform camera;
+        public float strength = 2F;
+        public float falloff = .25F;
+        
+        private float shake;
+
+        private void Update()
+        {
+            if(!camera)
+                return;
+
+            Vector3 cam = camera.localPosition;
+
+            cam.x = Random.Range(0F, shake);
+            cam.y = Random.Range(0F, shake);
+            
+            camera.localPosition = cam;
+            
+            if (shake >= 0F)
+                shake -= falloff * Time.deltaTime;
+        }
+
+        [UsedImplicitly] // Animator event
+        private void Step()
+        {
+            shake = strength;
+        }
+    }
+}
