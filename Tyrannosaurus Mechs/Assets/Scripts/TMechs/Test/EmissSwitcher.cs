85a46d5b1de08b73b9e9ca2dec60a0d16ad1f167
Arthur Uzulin <fuj1nman@hotmail.com>
2019-06-07 20:45:32 +1000

    Fixed levers

---
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Test/EmissSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using TMechs.Environment;
+using UnityEngine;
+
+namespace TMechs.Test
+{
+    public class EmissSwitcher : MonoBehaviour
+    {
+        [ColorUsage(false, true)]
+        public Color onColor = Color.green;
+        [ColorUsage(false, true)]
+        public Color offColor = Color.red;
+
+        private Renderer render;
+        private static readonly int EMISSIVE_COLOR = Shader.PropertyToID("_EmissiveColor");
+
+        private void Awake()
+        {
+            render = GetComponent<Renderer>();
+
+            if (render)
+                render.material.SetColor(EMISSIVE_COLOR, offColor);
+        }
+
+        public void UpdateColor()
+        {
+            render.material.SetColor(EMISSIVE_COLOR, InteractableObject.eventToggleValue ? onColor : offColor);
+        }
+    }
+}
