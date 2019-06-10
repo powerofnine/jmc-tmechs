a7cdbe0fb153e84c482176feb1a6064632eac8c2
Arthur Uzulin <fuj1nman@hotmail.com>
2019-06-10 16:24:32 +1000

    Added shoulder charge damage to the dash

---
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Volumes/OutlinePostProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace TMechs.Volumes
+{
+    [Serializable, VolumeComponentMenu("Post-processing/TMechs/Outline")]
+    public class OutlinePostProcess : VolumeComponent
+    {
+        public ColorParameter outlineColor = new ColorParameter(Color.black);
+        public ClampedFloatParameter normalOutlineMultiplier = new ClampedFloatParameter(1F, 0F, 4F);
+        public ClampedFloatParameter normalOutlineBias = new ClampedFloatParameter(1F, 1F, 4F);
+        public ClampedFloatParameter depthOutlineMultiplier = new ClampedFloatParameter(1F, 0F, 4F);
+        public ClampedFloatParameter depthOutlineBias = new ClampedFloatParameter(1F, 1F, 4F);
+        
+        
+    }
+}
