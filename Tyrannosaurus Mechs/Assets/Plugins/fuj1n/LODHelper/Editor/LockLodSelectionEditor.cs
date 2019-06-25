eafe3fa24012aaf7391e5eb798b6727c73d641da
Arthur Uzulin <fuj1nman@hotmail.com>
2019-06-25 21:47:28 +1000

    LOD Helper locked all LOD components from being selected

---
+++ Tyrannosaurus Mechs/Assets/Plugins/fuj1n/LODHelper/Editor/LockLodSelectionEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LockLodSelection))]
+[CanEditMultipleObjects]
+public class LockLodSelectionEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        LockLodSelection trg = target as LockLodSelection;
+
+        if (!trg)
+            return;
+
+        Selection.objects = Selection.objects.Select(s =>
+        {
+            GameObject go = s as GameObject;
+
+            if (!go)
+                return s;
+
+            if (!go.GetComponent<LockLodSelection>())
+                return s;
+
+            return go.GetComponentInParent<LODGroup>().gameObject;
+        }).ToArray();
+    }
+}
