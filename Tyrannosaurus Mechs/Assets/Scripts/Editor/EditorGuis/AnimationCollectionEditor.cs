3607693c34d0bf31141f93a509e8f4ed8615a532
Arthur Uzulin <fuj1nman@hotmail.com>
2019-07-05 23:06:55 +1000

    Animation collection

---
+++ Tyrannosaurus Mechs/Assets/Scripts/Editor/EditorGuis/AnimationCollectionEditor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using TMechs.Animation;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.EditorGuis
+{
+    [CustomEditor(typeof(AnimationCollection))]
+    public class AnimationCollectionEditor : UnityEditor.Editor
+    {
+        private static Type[] availableTypes = FetchTypes();
+        
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            SerializedProperty type = serializedObject.FindProperty("serializedType");
+            SerializedProperty clips = serializedObject.FindProperty("animations");
+
+            EditorGUILayout.BeginVertical();
+            
+            Type typeVal = DrawTypeDropdown(type);
+            DrawAnimationsList(typeVal, clips);
+            
+            EditorGUILayout.EndVertical();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        public Type DrawTypeDropdown(SerializedProperty type)
+        {
+            EditorGUILayout.BeginHorizontal();
+            Type typeVal = AnimationCollection.ParseType(type.stringValue);
+
+            string[] stringTypes = availableTypes.Select(x => x.FullName).Prepend("None").ToArray();
+
+            int index = typeVal != null ? Array.IndexOf(availableTypes, typeVal) : -1;
+            
+            int selection = EditorGUILayout.Popup("Type", index + 1, stringTypes);
+
+            if (selection != index + 1)
+                type.stringValue = selection == 0 ? null : availableTypes[selection - 1].AssemblyQualifiedName;
+
+            if (GUILayout.Button(EditorGUIUtility.IconContent("d_Refresh"), GUILayout.Width(EditorGUIUtility.singleLineHeight * 2F), GUILayout.Height(EditorGUIUtility.singleLineHeight - 1F)))
+                availableTypes = FetchTypes();
+            
+            EditorGUILayout.EndHorizontal();
+            
+            return typeVal;
+        }
+
+        public void DrawAnimationsList(Type typeVal, SerializedProperty clips)
+        {
+            if (typeVal == null)
+            {
+                EditorGUILayout.HelpBox("You must select a type before specifying animator clips", MessageType.Info);
+                return;
+            }
+
+            EditorGUILayout.LabelField("Clips", EditorStyles.boldLabel);
+            
+            string[] vals = Enum.GetNames(typeVal);
+
+            for (int i = 0; i < clips.arraySize && i < vals.Length; i++)
+                EditorGUILayout.PropertyField(clips.GetArrayElementAtIndex(i), new GUIContent(vals[i]));
+        }
+
+        private static Type[] FetchTypes() => 
+                (from a in AppDomain.CurrentDomain.GetAssemblies()
+                from t in a.GetTypes()
+                where t.IsEnum && t.GetCustomAttribute(typeof(AnimationCollection.EnumAttribute)) != null
+                select t).ToArray();
+    }
+}
---
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Animation/AnimationCollection.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TMechs.Animation
+{
+    [CreateAssetMenu(menuName = "Animation Collection", fileName = "Animations", order = 400)]
+    public class AnimationCollection : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [SerializeField]
+        private string serializedType;
+        [SerializeField]
+        private AnimationClip[] animations;
+
+        private Type enumType;
+        private Dictionary<Enum, AnimationClip> valueMap;
+
+        public bool IsType(Type type)
+        {
+            return enumType == type;
+        }
+
+        public AnimationClip GetClip(Enum at)
+        {
+            if (!valueMap.ContainsKey(at))
+                return null;
+
+            return valueMap[at];
+        }
+        
+        public void OnBeforeSerialize()
+        {
+            if (enumType == null)
+                return;
+            
+            serializedType = enumType.AssemblyQualifiedName;
+        }
+
+        public void OnAfterDeserialize()
+        {
+            enumType = ParseType(serializedType);
+
+            if (enumType == null)
+                return;
+            
+            int len = Enum.GetValues(enumType).Length;
+            
+            if(animations == null)
+                animations = new AnimationClip[len];
+            else if(animations.Length != len)
+                Array.Resize(ref animations, len);
+
+            Enum[] vals = Enum.GetValues(enumType).Cast<Enum>().ToArray();
+            valueMap = Enumerable.Range(0, vals.Length).ToDictionary(x => vals[x], x => animations[x]);
+        }
+
+        public static Type ParseType(string type)
+        {
+            if (string.IsNullOrWhiteSpace(type))
+                return null;
+            
+            Type enumType = Type.GetType(type);
+            if(enumType == null)
+                throw new ArgumentException($"Specified type {type} cannot be found");
+            if (!enumType.IsEnum)
+                throw new ArgumentException($"Specified type {type} is not enum");
+
+            return enumType;
+        }
+
+        [AttributeUsage(AttributeTargets.Enum)]
+        public sealed class EnumAttribute : Attribute
+        {}
+
+        public sealed class ValidateAttribute : PropertyAttribute
+        {
+            public readonly Type type;
+
+            public ValidateAttribute(Type type)
+            {
+                this.type = type;
+            }
+        }
+    }
+}
