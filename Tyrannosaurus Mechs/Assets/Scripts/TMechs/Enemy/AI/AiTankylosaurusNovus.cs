82c0190288f2ef96f7b18243613145f5ab000e00
Arthur Uzulin <fuj1nman@hotmail.com>
2019-07-21 20:16:46 +1000

    Pickupable rock tweaks

--- Tyrannosaurus Mechs/Assets/Scripts/TMechs/Enemy/AI/AiTankylosaurusNovus.cs
+++ Tyrannosaurus Mechs/Assets/Scripts/TMechs/Enemy/AI/AiTankylosaurusNovus.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using Animancer;
+using UnityEngine;
 
 namespace TMechs.Enemy.AI
 {
     public class AiTankylosaurusNovus : MonoBehaviour
     {
-        
+        private void Start()
+        {
+            
+        }
+
+        private class TankyloShared
+        {
+            public EventfulAnimancerComponent animancer;
+        }
     }
 }
